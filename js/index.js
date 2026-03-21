bd9935b714d3f194efe2fde763a9074aea5ad3ef
Lamphrangmi <lamphrangmilamin46169@gmail.com>
2026-03-22 00:47:41 +0530

    feat: render review cards from product-reviews.json

--- js/index.js
+++ js/index.js
@@ -1,2 +1,58 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
+import reviews from "../data/product-reviews.json" with {type: "json"};
+// console.log(reviews);
+import users from "../data/users.json" with {type: "json"};
+// console.log(users);
+
+const cardsContainer = document.querySelector('.cards-container');
+// console.log(cardsContainer);
+
+function renderCards(productReviews, perPage = 12) {
+    // 
+}
+
+const cardHTML = reviews.map(review => {
+    const username = users.find(user => user.user_id === review.user_id).name;
+    // console.log(username);
+    const avatarUrl = users.find(user => user.user_id === review.user_id).avatar_url;
+    // console.log(avatarUrl)
+    function formatDate(dateInput) {
+        const date = new Date(dateInput + 'T00:00:00');
+        const newDate = date.toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        });
+        return newDate;
+    }
+    
+    return `<div class="card-container flex flex-col gap-4">
+            <div class="flex gap-4">
+              <div class="w-12 h-12">
+                <img class="w-12 h-12 object-cover rounded-full" src="${avatarUrl}" alt="avatar">
+              </div>
+              <div class="review-message-container flex flex-grow justify-between">
+                <div class="flex flex-col justify-between">
+                  <p class="font-semibold text-neutral-900">${username}</p>
+                  <span>
+                    <i class="ri-star-fill text-yellow-400 text-xl"></i>
+                    <i class="ri-star-fill text-yellow-400 text-xl"></i>
+                    <i class="ri-star-fill text-yellow-400 text-xl"></i>
+                    <i class="ri-star-fill text-yellow-400 text-xl"></i>
+                    <i class="ri-star-fill text-gray-200 text-xl"></i>
+                  </span>
+                </div>
+                <div class="date-n-time">
+                  <span class="text-neutral-600 text-xs">${formatDate(review.created_at)}</span>
+                </div>
+              </div>
+            </div>
+            <div class="text-neutral-600">
+              <p>${review.content}</p>
+            </div>
+          </div>`
+}).join("");
+
+// console.log(cardHTML);
+
+cardsContainer.innerHTML = cardHTML;
+
