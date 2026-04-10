60548db11fe9be6fb58e955bc1f44a17551c4cb7
Lamphrangmi <lamphrangmilamin46169@gmail.com>
2026-04-10 21:33:25 +0530

    refactor: improve code structure and readability
    - group code according to their generic purpose
    - change function names
    - create resetVisibleCount helper function
    - remove redundant code wherever necessary

--- js/index.js
+++ js/index.js
@@ -1,90 +1,121 @@
 import reviews from "../data/product-reviews.json" with { type: "json" };
 import users from "../data/users.json" with { type: "json" };
-const cardsContainer = document.querySelector(".cards-container");
-const remainReviewsContainer = document.getElementById("remaining-reviews");
-const showMoreBtn = document.getElementById("show-more-btn");
 
 // State
 let sortedReviews = [...reviews].sort(
   (a, b) => new Date(b.created_at) - new Date(a.created_at),
 );
 
-// Pagination
+const cardsContainer = document.querySelector(".cards-container");
+const remainingReviewsCount = document.getElementById("remaining-reviews");
+const showMoreBtn = document.getElementById("show-more-btn");
+// Overall rating
+const overallRating = document.getElementById("overallRating");
+const averageRatingContainer = document.getElementById("averageRating");
+
+const fiveStarsPercent = document.getElementById("fiveStarsPercent");
+const fiveStarsBand = document.getElementById("fiveStarsBand");
+fiveStarsPercent.innerText = renderPercent(5);
+fiveStarsBand.classList.add(`w-[${renderPercent(5)}]`);
+
+const fourStarsPercent = document.getElementById("fourStarsPercent");
+const fourStarsBand = document.getElementById("fourStarsBand");
+fourStarsPercent.innerText = renderPercent(4);
+fourStarsBand.classList.add(`w-[${renderPercent(4)}]`);
+
+const threeStarsPercent = document.getElementById("threeStarsPercent");
+const threeStarsBand = document.getElementById("threeStarsBand");
+threeStarsPercent.innerText = renderPercent(3);
+threeStarsBand.classList.add(`w-[${renderPercent(3)}]`);
+
+const twoStarsBand = document.getElementById("twoStarsBand");
+const twoStarsPercent = document.getElementById("twoStarsPercent");
+twoStarsPercent.innerText = renderPercent(2);
+twoStarsBand.classList.add(`w-[${renderPercent(2)}]`);
+
+const oneStarsBand = document.getElementById("oneStarsBand");
+const oneStarsPercent = document.getElementById("oneStarsPercent");
+oneStarsPercent.innerText = renderPercent(1);
+oneStarsBand.classList.add(`w-[${renderPercent(1)}]`);
+
+const ratingBands = document.querySelectorAll(".ratingBandContainer");
+const clearFilterBtn = document.getElementById("clearFilterBtn");
+const ratingBandLabels = document.querySelectorAll(".rating-band-label");
+
+// RATING SUMMARY
+const totalRatings = reviews.reduce(
+  (accumulator, review) => accumulator + review.rating,
+  0,
+);
+const averageRating = totalRatings / reviews.length;
+const roundedAverage = averageRating.toFixed(1);
+
+overallRating.innerHTML = renderRating(averageRating);
+averageRatingContainer.innerText = roundedAverage;
+
+// Rendering total number of reviews
+document.getElementById("total-no-reviews").innerText = sortedReviews.length;
+
 let visibleReviews = [];
 
-const mql = window.matchMedia("(width <= 768px)");
+const mobileMediaQuery = window.matchMedia("(width <= 768px)");
 
-let visibleCount = mql.matches ? 10 : 12;
+let visibleCount = mobileMediaQuery.matches ? 10 : 12;
 
 // initial value of pageSize as soon as the page/app loads
-let pageSize = mql.matches ? 10 : 12;
+let pageSize = mobileMediaQuery.matches ? 10 : 12;
 
 let activeRatingFilter = null;
-let filteredReviews = getFilteredReviews();
+let activeReviews = getFilteredReviews();
 
-mql.addEventListener("change", (event) => {
+// Initial rendering
+renderReviewsPage(visibleCount);
+
+mobileMediaQuery.addEventListener("change", (event) => {
   pageSize = event.matches ? 10 : 12;
-  visibleCount = Math.min(filteredReviews.length, pageSize);
-  pagination(visibleCount);
+  visibleCount = resetVisibleCount();
+  renderReviewsPage(visibleCount);
 });
-
-function pagination(count) {
-  visibleReviews = filteredReviews.slice(0, count);
-  cardsContainer.innerHTML = cardHTML(visibleReviews);
-  remainReviewsContainer.innerText = `${filteredReviews.length - visibleReviews.length}`;
-}
-pagination(visibleCount);
 
 // Show more button
 showMoreBtn.addEventListener("click", () => {
-  visibleCount = Math.min(visibleCount + pageSize, filteredReviews.length);
-  pagination(visibleCount);
+  visibleCount = Math.min(visibleCount + pageSize, activeReviews.length);
+  renderReviewsPage(visibleCount);
 });
 
-// Date formatting helper function
-function formatDate(dateInput) {
-  const date = new Date(dateInput + "T00:00:00");
-  const newDate = date.toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
+ratingBands.forEach((band) => {
+  band.addEventListener("click", () => {
+    clearFilterBtn.classList.remove("hidden");
+    activeRatingFilter = Number(band.dataset.rating);
+    activeReviews = getFilteredReviews();
+    visibleCount = resetVisibleCount();
+    renderReviewsPage(visibleCount);
+
+
+    // active state logic
+    ratingBandLabels.forEach(label => {
+      label.classList.remove('text-indigo-700');
+      label.classList.add('text-neutral-600');
+      label.classList.add('hover:text-neutral-900');
+    })
+
+    band.querySelector('.rating-band-label').classList.remove('text-neutral-600');
+    band.querySelector('.rating-band-label').classList.remove('hover:text-neutral-900');
+    band.querySelector('.rating-band-label').classList.remove('text-neutral-600');
+    band.querySelector('.rating-band-label').classList.add('text-indigo-700');
   });
-  return newDate;
-}
+});
 
-// Render rating bar helper function
-function renderRating(rating) {
-  let ratingMarkup = ``;
-  const totalStars = 5;
-  let fullStars = Number(Math.floor(rating));
-  const decimalPart = Number((rating % 1).toFixed(1));
-  let halfStars = 0;
-
-  // render full stars markup
-  for (let i = 0; i < fullStars; i++) {
-    ratingMarkup += `<i class="ri-star-fill text-yellow-400 text-xl"></i>`;
-  }
-
-  // render half stars markup
-  if (decimalPart >= 0.1 && decimalPart <= 0.5) {
-    ratingMarkup += `<i class="ri-star-half-fill text-yellow-400 text-xl"></i>`;
-    halfStars = 1;
-  } else if (decimalPart >= 0.6 && decimalPart <= 0.9) {
-    ratingMarkup += `<i class="ri-star-fill text-yellow-400 text-xl"></i>`;
-    fullStars += 1;
-  }
-
-  const emptyStars = totalStars - fullStars - halfStars;
-  // render empty stars markup
-  for (let i = 0; i < emptyStars; i++) {
-    ratingMarkup += `<i class="ri-star-fill text-gray-200 text-xl"></i>`;
-  }
-
-  return ratingMarkup;
-}
+clearFilterBtn.addEventListener("click", () => {
+  clearFilterBtn.classList.add("hidden");
+  activeRatingFilter = null;
+  activeReviews = getFilteredReviews();
+  visibleCount = resetVisibleCount();
+  renderReviewsPage(visibleCount);
+})
 
 // Rendering Card Reviews markup
-function cardHTML(arrOfReviews) {
+function renderReviewCards(arrOfReviews) {
   return arrOfReviews
     .map((review) => {
       // Obj reviews variables
@@ -136,25 +167,36 @@ function cardHTML(arrOfReviews) {
     .join("");
 }
 
-cardsContainer.innerHTML = cardHTML(visibleReviews);
+// Render rating bar helper function
+function renderRating(rating) {
+  let ratingMarkup = ``;
+  const totalStars = 5;
+  let fullStars = Number(Math.floor(rating));
+  const decimalPart = Number((rating % 1).toFixed(1));
+  let halfStars = 0;
 
-// RATING SUMMARY
-// Overall rating
-const overallRating = document.getElementById("overallRating");
-const averageRatingContainer = document.getElementById("averageRating");
+  // render full stars markup
+  for (let i = 0; i < fullStars; i++) {
+    ratingMarkup += `<i class="ri-star-fill text-yellow-400 text-xl"></i>`;
+  }
 
-const totalRatings = reviews.reduce(
-  (accumulator, review) => accumulator + review.rating,
-  0,
-);
-const averageRating = totalRatings / reviews.length;
-const roundedAverage = averageRating.toFixed(1);
+  // render half stars markup
+  if (decimalPart >= 0.1 && decimalPart <= 0.5) {
+    ratingMarkup += `<i class="ri-star-half-fill text-yellow-400 text-xl"></i>`;
+    halfStars = 1;
+  } else if (decimalPart >= 0.6 && decimalPart <= 0.9) {
+    ratingMarkup += `<i class="ri-star-fill text-yellow-400 text-xl"></i>`;
+    fullStars += 1;
+  }
 
-overallRating.innerHTML = renderRating(averageRating);
-averageRatingContainer.innerText = roundedAverage;
+  const emptyStars = totalStars - fullStars - halfStars;
+  // render empty stars markup
+  for (let i = 0; i < emptyStars; i++) {
+    ratingMarkup += `<i class="ri-star-fill text-gray-200 text-xl"></i>`;
+  }
 
-// Rendering total number of reviews
-document.getElementById("total-no-reviews").innerText = sortedReviews.length;
+  return ratingMarkup;
+}
 
 // Review Bands
 function renderPercent(numberOfStars) {
@@ -166,70 +208,32 @@ function renderPercent(numberOfStars) {
   return roundedPercent + "%";
 }
 
-const fiveStarsPercent = document.getElementById("fiveStarsPercent");
-const fiveStarsBand = document.getElementById("fiveStarsBand");
-fiveStarsPercent.innerText = renderPercent(5);
-fiveStarsBand.classList.add(`w-[${renderPercent(5)}]`);
+function renderReviewsPage(count) {
+  visibleReviews = activeReviews.slice(0, count);
+  cardsContainer.innerHTML = renderReviewCards(visibleReviews);
+  remainingReviewsCount.innerText = `${activeReviews.length - visibleReviews.length}`;
+}
 
-const fourStarsPercent = document.getElementById("fourStarsPercent");
-const fourStarsBand = document.getElementById("fourStarsBand");
-fourStarsPercent.innerText = renderPercent(4);
-fourStarsBand.classList.add(`w-[${renderPercent(4)}]`);
+// Date formatting helper function
+function formatDate(dateInput) {
+  const date = new Date(dateInput + "T00:00:00");
+  const newDate = date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  return newDate;
+}
 
-const threeStarsPercent = document.getElementById("threeStarsPercent");
-const threeStarsBand = document.getElementById("threeStarsBand");
-threeStarsPercent.innerText = renderPercent(3);
-threeStarsBand.classList.add(`w-[${renderPercent(3)}]`);
-
-const twoStarsBand = document.getElementById("twoStarsBand");
-const twoStarsPercent = document.getElementById("twoStarsPercent");
-twoStarsPercent.innerText = renderPercent(2);
-twoStarsBand.classList.add(`w-[${renderPercent(2)}]`);
-
-const oneStarsBand = document.getElementById("oneStarsBand");
-const oneStarsPercent = document.getElementById("oneStarsPercent");
-oneStarsPercent.innerText = renderPercent(1);
-oneStarsBand.classList.add(`w-[${renderPercent(1)}]`);
-
-const ratingBands = document.querySelectorAll(".ratingBandContainer");
-const clearFilterBtn = document.getElementById("clearFilterBtn");
-const ratingBandLabels = document.querySelectorAll(".rating-band-label");
-
+// Helper function
 function getFilteredReviews() {
   if (activeRatingFilter === null) return sortedReviews;
   return sortedReviews.filter((review) => review.rating === activeRatingFilter);
 }
 
-ratingBands.forEach((band) => {
-  band.addEventListener("click", () => {
-    clearFilterBtn.classList.remove("hidden");
-    activeRatingFilter = Number(band.dataset.rating);
-    filteredReviews = getFilteredReviews();
-    // if filtered array is less than pageSize, set visibleCount to a value equal to that array length
-    visibleCount =
-      filteredReviews.length < pageSize ? filteredReviews.length : pageSize;
-    pagination(visibleCount);
-
-    // active state logic
-    ratingBandLabels.forEach(label => {
-      label.classList.remove('text-indigo-700');
-      label.classList.add('text-neutral-600');
-      label.classList.add('hover:text-neutral-900');
-    })
-
-    band.querySelector('.rating-band-label').classList.remove('text-neutral-600');
-    band.querySelector('.rating-band-label').classList.remove('hover:text-neutral-900');
-    band.querySelector('.rating-band-label').classList.remove('text-neutral-600');
-    band.querySelector('.rating-band-label').classList.add('text-indigo-700');
-  });
-});
-
-clearFilterBtn.addEventListener("click", () => {
-  clearFilterBtn.classList.add("hidden");
-  activeRatingFilter = null;
-  filteredReviews = getFilteredReviews();
-  pagination(pageSize);
-})
+function resetVisibleCount() {
+  return Math.min(activeReviews.length, pageSize);
+}
 
 // Helper function to get the initials of a username of upto 2 characters long
 function getInitials(name = 'brenton mosley') {
